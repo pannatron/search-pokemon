153d03ccd51b18bdd0fb32034a60329024319915
pannatorn <57486797+pannatron@users.noreply.github.com>
2025-01-18 02:13:53 +0700

    refactor: optimize ApolloWrapper by using memoized Apollo client
    - Switched to `getClient()` with `useMemo` to ensure the Apollo client is only initialized once.
    - Improved performance by avoiding unnecessary re-creation of the client.

--- components/ApolloWrapper.tsx
+++ components/ApolloWrapper.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { ApolloProvider } from '@apollo/client';
-import client from '../graphql/client';
+import { getClient } from '../graphql/client';
+import { useMemo } from 'react';
 
 export default function ApolloWrapper({ children }: { children: React.ReactNode }) {
+  const client = useMemo(() => getClient(), []);
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 }
