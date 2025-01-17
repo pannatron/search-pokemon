a0b299ccb66b8df416b2071f882e75ccd8be6b37
pannatorn <57486797+pannatron@users.noreply.github.com>
2025-01-17 21:22:21 +0700

    feat: update GET_POKEMON query to fetch complete Pokémon details

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -6,17 +6,41 @@ export const GET_POKEMON: TypedDocumentNode<PokemonData, PokemonVars> = gql`
   query GetPokemon($name: String!) {
     pokemon(name: $name) {
       id
+      number
       name
+      weight {
+        minimum
+        maximum
+      }
+      height {
+        minimum
+        maximum
+      }
+      classification
       types
+      resistant
       attacks {
         fast {
           name
           damage
         }
+        special {
+          name
+          damage
+        }
       }
+      weaknesses
+      fleeRate
+      maxCP
+      maxHP
       evolutions {
         name
       }
+      evolutionRequirements {
+        amount
+        name
+      }
+      image
     }
   }
 `;
