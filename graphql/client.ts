c11a035593a175657a76c4e05ea3075e20ae6006
pannatorn <57486797+pannatron@users.noreply.github.com>
2025-01-18 02:15:58 +0700

    Refactor Apollo Client setup to support SSR and optimize cache management
    - Added SSR support with `ssrMode` in Apollo Client configuration.
    - Introduced `createApolloClient` function for cleaner and reusable client creation.
    - Implemented `getClient` function to handle client reuse in Client-Side and create new instances in Server-Side.
    - Enhanced cache management with `keyArgs` and `merge` for `pokemon` field.
    - Improved performance and code structure for better maintainability.

--- graphql/client.ts
+++ graphql/client.ts
@@ -1,30 +1,46 @@
-import { ApolloClient, InMemoryCache, defaultDataIdFromObject } from '@apollo/client';
+import { ApolloClient, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
 
-const cache = new InMemoryCache({
-  typePolicies: {
-    Query: {
-      fields: {
-        pokemon: {
-          merge: false,
+let client: ApolloClient<NormalizedCacheObject> | undefined;
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    uri: 'https://graphql-pokemon2.vercel.app/',
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            pokemon: {
+              keyArgs: ['name'],
+              merge: true,
+            },
+          },
         },
       },
+    }),
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: 'cache-first',
+        nextFetchPolicy: 'cache-first',
+      },
+      query: {
+        fetchPolicy: 'cache-first',
+        errorPolicy: 'all',
+      },
     },
-  },
-});
+    ssrMode: typeof window === 'undefined', // Enable SSR mode when running on server
+  });
+};
 
-const client = new ApolloClient({
-  uri: 'https://graphql-pokemon2.vercel.app/',
-  cache,
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: 'cache-first', 
-      nextFetchPolicy: 'cache-first',
-    },
-    query: {
-      fetchPolicy: 'cache-first',
-      errorPolicy: 'all',
-    },
-  },
-});
+export function getClient() {
+  // Create a new client for SSR
+  if (typeof window === 'undefined') {
+    return createApolloClient();
+  }
 
-export default client;
+  // Reuse client on the client-side
+  if (!client) {
+    client = createApolloClient();
+  }
+
+  return client;
+}
